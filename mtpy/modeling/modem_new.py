6fde6b78bc03efc481cca621e0053467d60a1fb8
ak <u64125@PC-65328.prod.lan>
2017-02-03 17:42:00 +1100

    updates to modem_new (projections) and utils.utmlatlongconversions. Also adding modules to create gmt maps (not finished yet!)

--- mtpy/modeling/modem_new.py
+++ mtpy/modeling/modem_new.py
@@ -593,7 +593,7 @@ class Data(object):
         self.center_position_EN = 0.5*np.array([self.data_array['east'].min() + self.data_array['east'].max(),
                                                 self.data_array['north'].min() + self.data_array['north'].max()])
         # try to update center_position by projecting center xy
-        self.project_xy(*self.center_position_EN)
+        self.center_position = self.project_xy(*self.center_position_EN)
 
         #remove the average distance to get coordinates in a relative space
         self.data_array['rel_east'] = self.data_array['east']-self.center_position_EN[0]
@@ -1075,6 +1075,11 @@ class Data(object):
         dfid = file(self.data_fn, 'w')
         dfid.writelines(dlines)
         dfid.close()
+
+        # write epsg and center position to a file, if they exist
+        np.savetxt(op.join(self.save_path,'center_position.dat'),self.center_position_EN,fmt='%.1f')
+        np.savetxt(op.join(self.save_path,'epsg'),np.array([self.epsg]),fmt='%1i')
+        
         
         print 'Wrote ModEM data file to {0}'.format(self.data_fn)
         
@@ -1927,7 +1932,7 @@ class Model(object):
 
 
 
-    def make_mesh(self, update_data_center=False):
+    def make_mesh(self, update_data_center=True):
         """ 
         create finite element mesh according to parameters set.
         
@@ -2079,8 +2084,11 @@ class Model(object):
         # if desired, update the data center position (need to first project 
         # east/north back to lat/lon) and rewrite to file
         if update_data_center:
-            self.Data.center_position = self.Data.project_xy(self.Data.center_position_EN[0],
-                                                             self.Data.center_position_EN[1])
+            try:
+                self.Data.center_position = self.Data.project_xy(self.Data.center_position_EN[0],
+                                                                 self.Data.center_position_EN[1])
+            except:
+                pass
             self.Data.write_data_file(compute_error=False,fill=False)
 
             
@@ -3008,28 +3016,70 @@ class Model(object):
 #        self.covariance_mask[self.res_model > 0.9*air_resistivity] = 0
 
 
-    def write_xyres(self,origin=[0,0],savepath=None,outfile_basename='DepthSlice'):
+    def write_xyres(self,location_type='EN',origin=[0,0],model_epsg=None,depth_index='all',
+                    savepath=None,outfile_basename='DepthSlice',log_res=False):
         """
         write files containing depth slice data (x, y, res for each depth)
+        
+        origin = x,y coordinate of zero point of ModEM_grid, or name of file
+                 containing this info (full path or relative to model files)
+        savepath = path to save to, default is the model object save path
+        location_type = 'EN' or 'LL' xy points saved as eastings/northings or 
+                        longitude/latitude, if 'LL' need to also provide model_epsg
+        model_epsg = epsg number that was used to project the model
+        outfile_basename = string for basename for saving the depth slices.
+        log_res = True/False - option to save resistivity values as log10 
+                               instead of linear
         
         """
         if savepath is None:
             savepath = self.save_path
             
         # make a directory to save the files
-        savepath = op.join(savepath,'DepthSlices')
+        savepath = op.join(savepath,outfile_basename)
         if not op.exists(savepath):
             os.mkdir(savepath)
+        
+        # try getting centre location info from file
+        if type(origin) == str:
+            try:
+                origin = np.loadtxt(origin)
+            except:
+                print "Please provide origin as a list, array or tuple or as a valid filename containing this info"
+                origin = [0,0]
         
         # reshape the data
         x,y,z = [np.mean([arr[1:], arr[:-1]],axis=0) for arr in \
                 [self.grid_east + origin[0], self.grid_north + origin[1], self.grid_z]]
         x,y = [arr.flatten() for arr in np.meshgrid(x,y)]
         
-        for k in range(len(z)):
+        # set format for saving data
+        fmt = ['%.1f','%.1f','%.3e']
+        
+        # convert to lat/long if needed
+        if location_type == 'LL':
+            if np.any(origin) == 0:
+                print "Warning, origin coordinates provided as zero, output lat/long are likely to be incorrect"
+            x,y = utm2ll.project(x,y,model_epsg,4326)
+            # update format to accommodate lat/lon
+            fmt[:2] = ['%.6f','%.6f']
+            
+        # make depth indices into a list
+        if depth_index == 'all':
+            depthindices = range(len(z))
+        elif np.iterable(depth_index):
+            depthindices = np.array(depth_index).astype(int)
+        else:
+            depthindices = [depth_index]
+        
+        for k in depthindices:
             fname = op.join(savepath,outfile_basename+'_%1im.xyz'%z[k])
-            data = np.vstack([x,y,self.res_model[:,:,k].flatten()]).T
-            np.savetxt(fname,data,fmt=['%.1f','%.1f','%.3e'])
+            vals = self.res_model[:,:,k].flatten()
+            if log_res:
+                vals = np.log10(vals)
+                fmt[-1] = '%.3f'
+            data = np.vstack([x,y,vals]).T
+            np.savetxt(fname,data,fmt=fmt)
         
 
 
@@ -7021,7 +7071,6 @@ class PlotPTMaps(mtplottools.MTEllipse):
                     
                 np.savetxt(filename,data_to_write,header=header,
                            fmt=['%.4e','%s','%.2f','%.2f','%.2f','%.2f','%.2f','%.3f'])
-
 
        
     def write_pt_data_to_gmt(self,period=None,epsg=None,savepath='.',center_utm=None,
